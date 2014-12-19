89800ad0985f17dc460af732e1e86af818f1c148
Andrew Douglas <andrew@spidergap.com>
2014-12-19 12:38:55 +0000

    Added basic rest apis

---
+++ src/Rasmus/PackageMateBundle/Controller/UsersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rasmus\PackageMateBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View as View;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UsersController extends Controller
+{
+  /**
+  * @Rest\View
+  * @QueryParam(name="query", description="Query param")
+  */
+  public function getAction(ParamFetcher $paramFetcher)
+  {
+    $query = $paramFetcher->get('query');
+    $view = View::create()
+    ->setStatusCode(200)
+    ->setData(array('query'=>$query))
+    ->setFormat('json');
+    return $this->get('fos_rest.view_handler')->handle($view);
+  }
+}
